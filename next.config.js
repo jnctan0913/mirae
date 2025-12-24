65d9e41487ac2194379e0830edac30bb5d51c89f
Dexabrain Deploy <dexabrain@gmail.com>
2025-12-24 15:04:30 +0800

    Update next.config.js for GitHub Pages static export

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: '/Mirae.ai',
-  assetPrefix: '/Mirae.ai',
 }
 
 module.exports = nextConfig
-
